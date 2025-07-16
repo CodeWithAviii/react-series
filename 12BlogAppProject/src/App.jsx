e8497978b981fa38800e3457bbf2985ba887da20
Arvind Kumar <arvindkum8312@gmail.com>
2025-07-17 00:30:46 +0530

    Redux part updated

--- 12BlogAppProject/src/App.jsx
+++ 12BlogAppProject/src/App.jsx
@@ -1,16 +1,44 @@
-
-import './App.css'
+import React, { useState, useEffect } from 'react'
+import authService from './appwrite/auth';
+import { useDispatch } from 'react-redux';
+import { login, logout } from './store/authSlice';
+// import './App.css'
 import conf from './conf/conf'
+import { Footer, Header } from './components';
+import { Outlet } from 'react-router-dom';
 
 function App() {
 
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect( () => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if(userData){
+          dispatch(login({userData}))
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => {
+        setLoading(false);
+      })
+  },[])
 
 
-  return (
-    <>
-      <h1>Blog App</h1>
-    </>
-  )
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap text-center content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App
---
+++ 12BlogAppProject/src/components/index.js
@@ -0,0 +1,6 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+
+export { Header, Footer };
+// This file serves as an index for exporting components from the components directory.
+// It allows for cleaner imports in other parts of the application.
