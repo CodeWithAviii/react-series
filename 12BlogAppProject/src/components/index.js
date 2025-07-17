835a93d929f8dae817f3c67fdc4493f2e837911d
Arvind Kumar <arvindkum8312@gmail.com>
2025-07-18 01:51:46 +0530

    Components structure updated

--- 12BlogAppProject/src/components/index.js
+++ 12BlogAppProject/src/components/index.js
@@ -1,6 +1,15 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
 
-export { Header, Footer };
+export { 
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn
+};
 // This file serves as an index for exporting components from the components directory.
 // It allows for cleaner imports in other parts of the application.
