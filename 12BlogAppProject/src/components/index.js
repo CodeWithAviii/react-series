5f54e712cbbcea6d5274e41dc686f8de312d20ae
Arvind Kumar <arvindkum8312@gmail.com>
2025-07-21 00:10:20 +0530

    lil update

--- 12BlogAppProject/src/components/index.js
+++ 12BlogAppProject/src/components/index.js
@@ -12,7 +12,10 @@ export {
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    Button,
+    Input
 };
 // This file serves as an index for exporting components from the components directory.
 // It allows for cleaner imports in other parts of the application.
+// You can add more components here as needed.
